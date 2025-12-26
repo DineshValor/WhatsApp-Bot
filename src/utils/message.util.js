27e390ba2acb5819cfaacf606a85e294f341bf3b
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-26 14:16:15 +0530

    Create message.util.js

---
+++ src/utils/message.util.js
@@ -0,0 +1,9 @@
+export function extractText(msg) {
+  return (
+    msg.message?.conversation ||
+    msg.message?.extendedTextMessage?.text ||
+    msg.message?.imageMessage?.caption ||
+    msg.message?.videoMessage?.caption ||
+    ''
+  ).trim()
+}
