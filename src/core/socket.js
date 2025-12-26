8f91d5f1825e6233f53f55dd6637f8bb980d94b3
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-26 16:45:47 +0530

    Update socket.js

--- src/core/socket.js
+++ src/core/socket.js
@@ -3,42 +3,29 @@ import makeWASocket, {
   DisconnectReason
 } from '@whiskeysockets/baileys'
 import Pino from 'pino'
-
 import { handleMessage } from '../handlers/message.handler.js'
 
-import qrcode from 'qrcode-terminal'
-
 export async function startSocket() {
-  console.log('🔌 startSocket() called')
-
   const { state, saveCreds } = await useMultiFileAuthState('auth')
-  console.log('🔐 auth state loaded')
 
   const sock = makeWASocket({
     auth: state,
     logger: Pino({ level: 'silent' }),
+    printQRInTerminal: true,
     markOnlineOnConnect: false
   })
 
   sock.ev.on('creds.update', saveCreds)
 
-  sock.ev.on('connection.update', ({ qr, connection }) => {
-  if (qr) {
-    qrcode.generate(qr, { small: true })
-  }
-  if (connection === 'open') {
-    console.log('✅ WhatsApp connected')
-  }
-})
-
+  sock.ev.on('connection.update', ({ connection, lastDisconnect }) => {
     if (connection === 'close') {
-      console.log('❌ Connection closed')
-
       const reason = lastDisconnect?.error?.output?.statusCode
       if (reason !== DisconnectReason.loggedOut) {
-        console.log('🔄 Reconnecting...')
         startSocket()
       }
+    }
+    if (connection === 'open') {
+      console.log('✅ WhatsApp connected')
     }
   })
 
