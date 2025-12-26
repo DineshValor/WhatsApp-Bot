980bf0a1d37c79c08adbc8d5b536b18af2faa7f2
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-26 18:02:10 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  prefix: process.env.PREFIX || '!',
-  botName: process.env.BOT_NAME || 'ValorBot',
+  prefix: process.env.PREFIX || '',
+  botName: process.env.BOT_NAME || '',
 
   // Comma-separated JIDs
   owners: (process.env.BOT_OWNERS || '')
