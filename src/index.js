d6e03ab947cbd7f0a484a8b4749f51488930f07b
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-26 14:15:24 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import { startSocket } from './core/socket.js'
+
+async function main() {
+  try {
+    await startSocket()
+  } catch (err) {
+    console.error('Fatal error:', err)
+    process.exit(1)
+  }
+}
+
+main()
